c6f949be7d3dc1a3eb4c47fb0e813a271e403759
sriraajj-lab <sriraajj@gmail.com>
2026-03-11 20:38:02 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Toaster } from "@/components/ui/toaster";
+import { InstallBanner } from "@/components/InstallBanner";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -35,6 +36,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
+      <InstallBanner />
       <BrowserRouter>
         <AuthProvider>
           <Routes>
